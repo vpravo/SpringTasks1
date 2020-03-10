a5f89a49b77cd42817643142d7ee85286a2d57e2
vpravo <yoursluzhba@gmail.com>
2020-03-10 15:43:10 +0300

    fix

--- spring-beans/src/main/java/app/model/AnimalsCage.java
+++ spring-beans/src/main/java/app/model/AnimalsCage.java
@@ -1,7 +1,6 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
